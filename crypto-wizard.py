777abb10df39d56556af3a02d783523ec4d561a7
Alexander Bittner <alexander.g.bittner@gmail.com>
2020-05-18 09:06:22 +0200

    added argument handling

---
+++ crypto-wizard.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+try:
+    mode_string = sys.argv[1]
+    mode = sys.argv[2]
+    pass
+except Exception as identifier:
+    print("error when reading parameters")
+    exit(1)
+    pass
+else:
+    pass
+
+
+if(mode_string != "--mode"):
+    print("USAGE: python crypto-wizard.py --mode [MODE]")
+    exit(1)
+
+if(mode=='ecc'):
+    #more stuff here
+
