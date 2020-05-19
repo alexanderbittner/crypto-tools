971db3523af4f8d46a1768837c1c0d4228d2fddd
Alexander Bittner <alexander.g.bittner@gmail.com>
2020-05-20 01:30:13 +0200

    fixed and commented ecc, integrated into crypto-wizard

--- ecc.py
+++ ecc.py
@@ -1,18 +1,21 @@
 from extended_euclid import Extended_Euclid
 
-
-
 class Curve:
+        # Curve constructor
         def __init__(self, a, b, p):
             self.param_a = a
             self.param_b = b
             self.param_p = p
 
 class Point:
+    # Point constructor
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
         self.is_neutral = False
+
+        # this point is a neutral element!
         if self.x == float('inf') and self.y == float('inf'):
             self.is_neutral = True
 
@@ -21,11 +24,16 @@ class Elliptic_Curves:
     def __init__(self):
         pass        
 
-    def check(self, verbosity, point, curve): #checks if the point is on the curve
+    # checks if a proved point is on a provided curve
+    def check(self, verbosity, point, curve):
         if verbosity>=1:
             print("[INFO]:    Checking if point ({}, {}) is on curve".format(point.x, point.y))
+
+        # can't be on the curve if point is neutral element
         if point.is_neutral == True:
             return 1
+
+        # check if point is actually on the curve, return result
         y = (point.y * point.y) % curve.param_p
         x = (point.x * point.x * point.x + curve.param_a * point.x + curve.param_b) % curve.param_p
         if y == x:
@@ -33,12 +41,17 @@ class Elliptic_Curves:
         else:
             return 0
     
+    # doubles a point on a provided curve and returns the new point as a point object
     def point_double(self, verbosity, point, curve):
         Ex = Extended_Euclid()
 
+        # make sure the point is on the curve before proceeding
         if self.check(verbosity, point, curve) == 0:
-            print("[ERROR]:   Point is not on curve.")
+            if(verbosity>=0):
+                print("[ERROR]:   Point is not on curve.")
             return 0
+
+        # first check for neutrality, then double the point & return the new point
         if point.is_neutral == True:
             new_x = float('inf')
             new_y = float('inf')
@@ -49,27 +62,27 @@ class Elliptic_Curves:
             new_y = (s*(point.x - new_x)-point.y) % curve.param_p
     
         point3 = Point(new_x, new_y)
-
-        if point3.is_neutral == True:
-            print("[INFO]:    Calculation done; New Point: O (neutral element)")
-        else :
-            print ("[INFO]:    Calculation done; New Point: (({}, {}))".format(point3.x, point3.y))
         return point3
 
-
+    # adds two points together on a provided curve and returns the new point as a point object
     def point_add(self, verbosity, point1, point2, curve):
         Ex = Extended_Euclid()
         k = 1
 
-        if self.check(verbosity, point1, curve) == 0: 
-            print("[ERROR]:   Point 1 is not on curve.")
+        # make sure the points are on the curve before proceeding
+        if self.check(verbosity, point1, curve) == 0:
+            if(verbosity>=0):
+                print("[ERROR]:   Point 1 is not on curve.")
             return(0)
         if self.check(verbosity, point2, curve) == 0:
-            print("[ERROR]:   Point 2 is not on curve.")
-            return(0)            
+            if(verbosity>=0):
+                print("[ERROR]:   Point 2 is not on curve.")
+            return(0)
+
         if(verbosity>=1):
             print("[INFO]:    Computing ({}, {}) + ({}, {})".format(point1.x, point1.y, point2.x, point2.y))
         
+        # first check for neutrality
         if point1.is_neutral == True:
             new_x = point2.x
             new_y = point2.y
@@ -79,14 +92,17 @@ class Elliptic_Curves:
             new_y = point1.y
             k = 0
         
+        # result is the neutral element
         if point1.x == point2.x and point1.y == (-point2.y)%curve.param_p:
             new_x = float('inf')
             new_y = float('inf')
             k = 0 
-    
+
+        # points are the same, so use point_double instead
         if point1.x == point2.x and point1.y == point2.y:
             return self.point_double(verbosity, point1, curve)
- 
+
+        # add the points & return the new point
         if k != 0:
             inv = Ex.eea(verbosity, ((point2.x - point1.x)%curve.param_p), curve.param_p)
             s = ((point2.y - point1.y)*inv) % curve.param_p
@@ -94,16 +110,12 @@ class Elliptic_Curves:
             new_y = (s*(point1.x - new_x)-point1.y) % curve.param_p
         
         point3 = Point(new_x, new_y)
-        
-        if point3.is_neutral == True:
-            print ("[INFO]:    Calculation done; New Point: O (neutral element)")
-        else:
-            print ("[INFO]:    Calculation done; New Point: ({}, {})".format(point3.x, point3.y))
-        
         return(point3)
 
+    # substracts two points from each other on a provided curve and returns the new point as a point object
     def point_substract(self, verbosity, point1, point2, curve):
         
+        # are the points on the curve?
         if self.check(verbosity, point1, curve) == 0: 
             print("[ERROR]:   Point 1 is not on curve.")
             return(0)
@@ -113,17 +125,11 @@ class Elliptic_Curves:
         
         if(verbosity>=1):
             print("[INFO]:    Computing ({}, {}) - ({}, {})".format(point1.x, point1.y, point2.x, point2.y))
+        
+        # check for neutrality, then modify second point & pass on to point_add
         if point2.is_neutral == True:
             neg_point2 = point2
         else:
             neg_point2 = Point(point2.x, (-point2.y % curve.param_p))
         
         return self.point_add(verbosity, point1, neg_point2, curve)
-
-pointA = Point(3, 36)
-PointB = Point(35, 17)
-PointC = Point(float('inf'), float('inf'))
-PointD = Point(148, 508)
-testcurve = Curve(41, 299, 1051)
-EC = Elliptic_Curves()
-EC.point_double(1, PointD, testcurve)
